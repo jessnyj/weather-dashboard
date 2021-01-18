df37dbd7011420ee7fbc58c1b0adc7d3a9ae54c6
jessny <jessnyjoseph@gmail.com>
2021-01-17 23:32:51 -0800

    added city weather icon

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // Today's Weather 
     var APIKey = "b2803d08257a2919cdac41bd8ceb3835"
 
     function displayWeather(citySearch) {
@@ -6,30 +7,23 @@ $(document).ready(function () {
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + APIKey,
             method: "GET"
         }).then(function (response) {
-            
-            console.log(response);
-
+            console.log(response)
             var tempNew = Math.round(((response.main.temp) - 273.15) * 9 / 5 + 32)
+            var weatherObj = response.weather[0].icon;
+            var weatherUrl = "http://openweathermap.org/img/wn/" + weatherObj + ".png";
 
-            // Transfer content to HTML
-            $("#cityName").html("<h1>" + response.name + " " + "(" + moment().format('L') + ")" + " " + "<i class='fa fa-cloud' style='font-size:35px;color:grey'></i></h1>");
+            $("#cityName").html("<h1>" + response.name + " " + "(" + moment().format('L') + ") " + "<img src=" + weatherUrl + ">" + "</h1>");
             var windSpeed = response.wind.speed;
             $("#temp").text("Temperature: " + tempNew + " °F");
             $("#humidity").text("Humidity: " + response.main.humidity + "%");
             $("#wind").text("Wind Speed: " + windSpeed + " MPH");
 
-            // Log the data in the console 
-            console.log("Temperature (F): " + response.main.temp);
-            console.log("Humidity: " + response.main.humidity);
-            console.log("Wind Speed: " + response.wind.speed);
-
             uvindex(response.coord.lon, response.coord.lat);
-            console.log(citySearch);
-
             forecast(response.coord.lon, response.coord.lat);
+
         });
     }
-
+/* <i class='fa fa-cloud' style='font-size:35px;color:#E8E8E8'></i> */
 
     // UV Index
     function uvindex(longitude, latidute) {
@@ -39,10 +33,9 @@ $(document).ready(function () {
             url: queryURL2,
             method: "GET"
         }).then(function (response) {
-            console.log(queryURL2);
-            console.log(response);
 
             $("#uvindex").text("UV Index: " + response.value);
+
         })
 
     }
@@ -58,20 +51,32 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
             console.log(response);
-            
+
             // for loop to display each day
             for (var i = 0; i < 5; i++) {
                 var dateCurrent = moment(response.daily[i].dt_txt).format('L');
                 console.log(dateCurrent);
                 var iconUrl = "http://openweathermap.org/img/wn/" + iconObj + ".png";
-                var iconObj = response.daily[0].weather[0].icon;
+                var iconObj = response.daily[i].weather[0].icon;
 
                 $(".date0").text(dateCurrent);
                 $(".icon0").html("<img src=" + iconUrl + ">");
-                $(".temp0").html("Temp: " + response.daily[0].temp.day + " °F")
-                $(".humidity0").html("Humidity: " + response.daily[0].humidity + "%")
+                $(".temp0").html("Temp: " + response.daily[i].temp.day + " °F");
+                $(".humidity0").html("Humidity: " + response.daily[i].humidity + "%");
+
+                // $("#forecast-append").append(`
+                // <div id="forecastCard" class="card text-white bg-primary mb-3" style="max-width: 10rem;">
+                //         <div class="forecast"></div>
+                //         <div class="body-forecast">
+                //             <p class="date0"></p>
+                //             <p class="icon0"></p>
+                //             <p class="temp0"></p>
+                //             <p class="humidity0">${response.daily[i].humidity}</p>
+                //         </div>
+                //     </div>
+                // `)
             }
-         })
+        })
 
     }
 
