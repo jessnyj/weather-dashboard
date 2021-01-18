3698e7e1347f8dcd88de3f5b4ec4ed7a158d4dfd
jessny <jessnyjoseph@gmail.com>
2021-01-17 21:44:17 -0800

    fixed day glitch

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     var APIKey = "b2803d08257a2919cdac41bd8ceb3835"
 
-
     function displayWeather(citySearch) {
         $.ajax({
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + APIKey,
@@ -13,7 +12,7 @@ $(document).ready(function () {
             var tempNew = Math.round(((response.main.temp) - 273.15) * 9 / 5 + 32)
 
             // Transfer content to HTML
-            $("#cityName").html("<h1>" + response.name + " " + moment().subtract(10, 'days').calendar() + " " + "<i class='fa fa-cloud' style='font-size:35px;color:blue'></i></h1>");
+            $("#cityName").html("<h1>" + response.name + " " + "(" + moment().format('L') + ")" + " " + "<i class='fa fa-cloud' style='font-size:35px;color:grey'></i></h1>");
             var windSpeed = response.wind.speed;
             $("#temp").text("Temperature (F) " + tempNew);
             $("#humidity").text("Humidity: " + response.main.humidity);
@@ -30,7 +29,6 @@ $(document).ready(function () {
             forecast(response.coord.lon, response.coord.lat);
         });
     }
-
 
 
     // UV Index
@@ -53,15 +51,22 @@ $(document).ready(function () {
     // 5day forecast
     var APIKey2 = "f0dce85278aa72524554b8d53e297782"
 
+
     function forecast(longitude, latidute) {
         $.ajax({
             url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + latidute + "&lon=" + longitude + "&exclude=hourly,minutely,current&appid=" + APIKey2 + "&units=imperial",
             method: "GET"
         }).then(function (response) {
             console.log(response);
+            
             // for loop to display each day
+            for (var i = 0; i < 5; i++) {
+                var dateCurrent = new Date((response.daily[i].dt)* 1000).toLocaleDateString();
+                $(".date0").text(dateCurrent);
+                $(".icon0").text(response.weather.icon)
+            }
+         })
 
-        });
     }
 
 
